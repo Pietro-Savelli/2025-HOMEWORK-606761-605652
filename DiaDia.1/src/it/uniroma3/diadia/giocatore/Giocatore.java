003b3865618376574e53b1dfdedcbb6729dd9fb1
luigicoscione <luigicoscione@8bc13abb-6817-4a50-b4b9-2cb583cd0ca6.fritz.box>
2025-03-26 10:08:40 +0100

    implementata la classe borsa nella classe giocatore

--- DiaDia.1/src/it/uniroma3/diadia/giocatore/Giocatore.java
+++ DiaDia.1/src/it/uniroma3/diadia/giocatore/Giocatore.java
@@ -37,4 +37,39 @@ public class Giocatore {
 	public void setCfu(int cfu) {
 		this.cfu = cfu;	
 	}
+	/**
+	 * Restituisce la borsa del giocatore
+	 * @return la borsa del giocatore
+	 */
+	public Borsa getBorsa() {
+		return this.borsa;
+	}
+	
+	/**
+	 * Aggiunge un attrezzo alla borsa del giocatore
+	 * @param attrezzo l'attrezzo da aggiungere
+	 * @return true se l'attrezzo è stato aggiunto, false altrimenti
+	 */
+	public boolean addAttrezzo(Attrezzo attrezzo) {
+		return this.borsa.addAttrezzo(attrezzo);
+	}
+	
+	/**
+	 * Rimuove un attrezzo dalla borsa del giocatore
+	 * @param nomeAttrezzo il nome dell'attrezzo da rimuovere
+	 * @return l'attrezzo rimosso, o null se non presente
+	 */
+	public Attrezzo removeAttrezzo(String nomeAttrezzo) {
+		return this.borsa.removeAttrezzo(nomeAttrezzo);
+	}
+	
+	/**
+	 * Controlla se un attrezzo è presente nella borsa
+	 * @param nomeAttrezzo il nome dell'attrezzo da cercare
+	 * @return true se l'attrezzo è presente, false altrimenti
+	 */
+	public boolean hasAttrezzo(String nomeAttrezzo) {
+		return this.borsa.hasAttrezzo(nomeAttrezzo);
+	}
 }
+
