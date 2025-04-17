9100e0959ac0009833e21104262c827ea8149521
Pietro_Savelli <pietrosavelli04@gmail.com>
2025-04-17 17:23:01 +0200

    test comandoVai

--- DiaDia.1/test/testComandi/ComandoVaiTest.java
+++ DiaDia.1/test/testComandi/ComandoVaiTest.java
@@ -1,5 +1,70 @@
 package testComandi;
 
-public class ComandoVaiTest {
+import static org.junit.Assert.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import it.uniroma3.diadia.Partita;
+import it.uniroma3.diadia.ambienti.Stanza;
+import it.uniroma3.diadia.comandi.Comando;
+import it.uniroma3.diadia.comandi.ComandoVai;
+
+public class ComandoVaiTest {
+	private Partita partita;
+	private Stanza atrio;
+	private Stanza biblioteca;
+	private Stanza n11;
+	private Comando c;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		c = new ComandoVai();
+		
+		this.partita = new Partita();
+		atrio = new Stanza("atrio");
+		biblioteca = new Stanza("biblioteca");
+		n11 = new Stanza("n11");
+		atrio.impostaStanzaAdiacente("nord", biblioteca);
+		atrio.impostaStanzaAdiacente("sud", n11);
+		biblioteca.impostaStanzaAdiacente("sud", atrio);
+		n11.impostaStanzaAdiacente("nord", biblioteca);
+		partita.setStanzaCorrente(atrio);
+	}
+
+	@Test
+	public void testEseguiDirezioneValida() {
+		c.setParametro("nord");
+		c.esegui(partita);
+		
+		assertEquals("biblioteca", partita.getStanzaCorrente().getNome());
+	}
+	
+	@Test
+	public void testEseguiDirezioniValide() {
+		c.setParametro("nord");
+		c.esegui(partita);
+		c.setParametro("sud");
+		c.esegui(partita);
+		c.setParametro("sud");
+		c.esegui(partita);
+		assertEquals("n11", partita.getStanzaCorrente().getNome());
+	}
+	
+	// rimani nella stessa stanza
+	@Test
+	public void testEseguiDirezioneNull() {
+		c.setParametro(null);
+		c.esegui(partita);
+		
+		assertEquals("atrio", partita.getStanzaCorrente().getNome());
+	}
+	
+	@Test
+	public void testEseguiDirezioneNonValida() {
+		c.setParametro("est");
+		c.esegui(partita);
+		
+		assertEquals("atrio", partita.getStanzaCorrente().getNome());
+	}
 }
